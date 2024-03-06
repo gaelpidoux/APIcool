69d83284d4cfa65f4c3c506bbf70f61a6e1686a0
gaelpidoux <geal.pidoux40@outlook.fr>
2024-03-06 09:17:11 +0100

    update3

--- config/bundles.php
+++ config/bundles.php
@@ -15,4 +15,5 @@ return [
     Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
     Gesdinet\JWTRefreshTokenBundle\GesdinetJWTRefreshTokenBundle::class => ['all' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
+    Gesdinet\JWTRefreshTokenBundle\GesdinetJWTRefreshTokenBundle::class => ['all' => true],
 ];
